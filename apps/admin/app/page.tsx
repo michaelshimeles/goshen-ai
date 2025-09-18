3842b71e2219298ea19629a34a85cf6197303d02
Michael Shimeles <michaelwasihun96@gmail.com>
2025-09-18 00:42:07 -0400

    start

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <p className="text-lg font-medium">Welcome to AI Inference</p>
+    </div>
+  );
+}
