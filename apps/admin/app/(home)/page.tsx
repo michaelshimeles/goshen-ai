fd6a7b9c481630fcf1851c07b5dd45969cf16c98
Michael Shimeles <michaelwasihun96@gmail.com>
2025-09-18 13:44:53 -0400

    clean up

--- apps/admin/app/(home)/page.tsx
+++ apps/admin/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { api } from "convex/_generated/api";
 import { preloadQuery } from "convex/nextjs";
-import HomePage from "./client";
+import HomePage from "./home";
 
 export default async function Page() {
     const waitlistTotal = await preloadQuery(api.waitlist.waitlistTotal);
